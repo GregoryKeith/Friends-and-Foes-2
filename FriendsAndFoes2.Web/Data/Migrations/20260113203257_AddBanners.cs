7321056c13c1d3b0c1f4744f02d2d4c146fc04d9
Greg Keith <gkeith@heeter.com>
2026-01-13 15:37:42 -0500

    Added banners migration

---
+++ FriendsAndFoes2.Web/Data/Migrations/20260113203257_AddBanners.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FriendsAndFoes2.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBanners : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Banners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Structure = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Domain = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    OwnerUserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banners", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Banners");
+        }
+    }
+}
